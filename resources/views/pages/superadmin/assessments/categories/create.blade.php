d3080d63ca2a3a6e3adef6d46d863efe38158a67
Clyde Ortega <jah.clydeortega14@gmail.com>
2021-03-10 17:23:20 +0800

    modified create category

--- resources/views/pages/superadmin/assessments/categories/create.blade.php
+++ resources/views/pages/superadmin/assessments/categories/create.blade.php
@@ -2,25 +2,53 @@
 
 @section('content')
 	
-	<div class="block-header">
-		<h2>Assessment Category</h2>
-	</div>
-
+	<h1 class="h3 mb-3 text-gray-800">Assessment Categories</h1>
 	{{ Breadcrumbs::render('categories.create') }}
 
 	<div class="row">
-		<div class="col-sm-12">
-			<div class="card">
+
+		<div class="col-sm-4">
+			<div class="card mb-3">
 				<div class="card-header">
-					create
+					category
 				</div>
 
 				<div class="card-body">
-					<div class="form-group row mt-4">
-						<label for="name" class="col-form-label col-sm-4 text-md-right">{{ __('Name') }}</label>
-						<div class="col-sm-6">
-							<input type="text" name="name" class="form-control">
-						</div>
+					<div class="form-group">
+						<label for="name">Name</label>
+						<input type="text" name="name" class="form-control">
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-sm-8">
+			<div class="card mb-3">
+				<div class="card-header">
+					questionnaiers
+				</div>
+
+				<div class="card-body">
+					<div class="d-sm-flex justify-content-end align-items-center mb-3">
+						<button class="btn btn-info btn-sm" type="button">
+							<i class="fa fa-plus"></i>
+						    <span>Add Question</span>
+					   </button>
+					</div>
+
+					<div class="table-responsive">
+						<table class="table table-striped">
+							<thead>
+								<tr>
+									<th>Question</th>
+									<th>Option</th>
+								</tr>
+							</thead>
+
+							<tbody>
+								
+							</tbody>
+						</table>
 					</div>
 				</div>
 			</div>
