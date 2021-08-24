8238d2312e8b5f29cad1cb7c6af1a3b2876b33fc
clydeortega <jah.clydeortega14@gmail.com>
2021-08-24 21:04:29 +0800

    modified encryption

--- public/js/app.js
+++ public/js/app.js
@@ -116816,13 +116816,13 @@ instance.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: 'ABCDEFG',
-  cluster: 'mt1',
+  key: "ABCDEFG",
+  cluster: "mt1",
   wsHost: window.location.hostname,
   wsPort: 6001,
   forceTLS: false,
   disabledStats: true,
-  encrypted: true
+  encrypted: false
 });
 
 /***/ }),
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -41,11 +41,11 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: 'ABCDEFG',
-    cluster: 'mt1',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort:6001,
     forceTLS: false,
     disabledStats: true,
-    encrypted: true
+    encrypted: false
 });
