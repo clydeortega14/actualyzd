4a982dbf260e7bf24531dfc9946cfb52096c8d26
clydeortega <jah.clydeortega14@gmail.com>
2023-02-04 18:10:08 +0800

    modified reschedule

--- resources/views/pages/bookings/reschedule.blade.php
+++ resources/views/pages/bookings/reschedule.blade.php
@@ -2,20 +2,34 @@
 
 @section('content')
 	
+
 	<div class="container-fluid">
 
-		<h1>Reschedule a booking</h1>
 		<a href="{{ route('home') }}" class="btn btn-outline-secondary mb-3">
 			<i class="fa fa-arrow-left"></i>
 			<span>Back to Home</span>
 		</a>
 
+		<h3 ></h3>
+		
+
 		<!-- Form for rescheduling -->
 		<div class="row">
+			<div class="col-md-12">
+				<div class="card mb-3">
+					<div class="card-header">
+						<h3 class="card-title text-center">Reschedule Session</h3>
+					</div>
+				</div>
+				
+			</div>
 			<div class="col-md-3">
 				<div class="card mb-3">
+					<div class="card-header">
+						Session Summary
+					</div>
 					<div class="card-body">
-						<h5 class="card-title text-secondary">Recent Booking Schedule Summary</h5>
+						
 
 						<ul class="list-group mt-3">
 							<li class="list-group-item d-flex justify-content-between align-items-center">
@@ -51,18 +65,18 @@
 
 			<div class="col-md-9">
 
-				<h3 class="mb-3">How ?</h3>
-				<ol class="mb-3 ml-3 border-bottom">
-					<li><p class="lead">Select a rescheduling date in the calendar below.</p></li>
-					<li><p class="lead">Upon clicking the date, a modal will shown that will request you to select the rescheduling time.</p></li>
-					<li><p class="lead">Then click submit.</p></li>
-				</ol>
-
-				<reschedule-calendar 
-					:booking="{{ $booking }}"
-					:user="{{ auth()->user() }}">
-						
-				</reschedule-calendar>
+				<div class="card">
+					<div class="card-header">
+						Choose Reschedule Date
+					</div>
+					<div class="card-body">
+						<reschedule-calendar 
+							:booking="{{ $booking }}"
+							:user="{{ auth()->user() }}">
+								
+						</reschedule-calendar>
+					</div>
+				</div>
 			</div>
 			
 		</div>
