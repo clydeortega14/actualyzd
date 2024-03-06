fd5effa1ca8a677111e75457b76ac55d4121ec9b
clyde <jah.clydeortega14@gmail.com>
2024-03-06 12:02:17 +0800

    default login command

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Hello Welcome TO Actualyzd') }}</div>
+                <div class="card-header">{{ __('Login Here) }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
